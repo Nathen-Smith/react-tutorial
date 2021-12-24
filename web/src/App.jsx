2ab384a5407459c6101c25bf7983986613699732
Nathen-Smith <nathencsmith@gmail.com>
2021-12-24 01:04:13 -0800

    hyperlink may be needed

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { Disclosure } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-
-  // ,
-  //   "lint": "eslint src/**/*.jsx --fix" 
+  return classes.filter(Boolean).join(" ");
 }
 
 const App = () => {
   const navigation = [
-    { name: 'Home', to: '/' },
-    { name: 'Search', to: '/search' },
-    { name: 'Gallery', to: '/gallery' },
+    { name: "Home", to: "/" },
+    { name: "Search", to: "/search" },
+    { name: "Gallery", to: "/gallery" },
+    { name: "Description", to: "/description" },
   ];
-  console.log(document.getElementById(window.location.pathname)?.offsetWidth)
 
   return (
     <div>
@@ -23,7 +20,6 @@ const App = () => {
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-              
               <div className="relative flex items-center justify-between h-16">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none">
@@ -38,22 +34,37 @@ const App = () => {
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="hidden sm:block sm">
                     <div className="flex space-x-4 ">
-                      {navigation.map((item) => (
-                        <a
-                          id={item.to}
-                          key={item.name}
-                          href={item.to}
-                          // style={{ boxShadow:
-                          // window.location.pathname === item.to ? 'inset 0 -2px 0 #1d4ed8' : '0' }}
-                          className={classNames(
-                            window.location.pathname === item.to
-                              ? 'text-black'
-                              : 'text-gray-400 hover:text-black',
-                            'h-16 text-sm font-medium text-center flex justify-center items-center',
-                          )}
-                        >
-                          {item.name}
-                        </a>
+                      {navigation.map((item, index) => (
+                        <span>
+                          <a
+                            id={item.to}
+                            key={item.name}
+                            href={item.to}
+                            className={classNames(
+                              window.location.pathname === item.to
+                                ? "text-black"
+                                : "text-gray-400 hover:text-black",
+                              "h-16 text-sm font-medium text-center flex justify-center items-center"
+                            )}
+                          >
+                            {item.name}
+                          </a>
+
+                          <div
+                            className="relative h-0.5 bg-blue-600 -top-0.5 rounded-t-lg"
+                            style={
+                              ({
+                                width: document.getElementById(item.to)
+                                  ?.offsetWidth,
+                              },
+                              { transition: "opacity 3s ease-in-out" },
+                              {
+                                opacity:
+                                  window.location.pathname === item.to ? 1 : 0,
+                              })
+                            }
+                          ></div>
+                        </span>
                       ))}
                     </div>
                   </div>
@@ -63,16 +74,7 @@ const App = () => {
                     <span>Nathen</span>
                   </div>
                 </div>
-                
               </div>
-              <div 
-                className="relative h-1 bg-blue-600 -top-1 w-10"
-                style={{width: document.getElementById(window.location.pathname)?.offsetWidth}}
-
-              >
-
-              </div>
-              
             </div>
             <Disclosure.Panel className="sm:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1 flex flex-col w-40">
@@ -82,9 +84,9 @@ const App = () => {
                     href={item.to}
                     className={classNames(
                       window.location.pathname === item.to
-                        ? 'bg-gray-300 text-black'
-                        : 'text-gray-400 hover:bg-gray-300 hover:text-black',
-                      'px-3 py-2 rounded-md text-sm font-medium mr-auto',
+                        ? "bg-gray-300 text-black"
+                        : "text-gray-400 hover:bg-gray-300 hover:text-black",
+                      "px-3 py-2 rounded-md text-sm font-medium mr-auto"
                     )}
                   >
                     {item.name}

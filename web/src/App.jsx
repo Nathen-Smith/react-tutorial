be346f78c513b20bcbca5d5fb38ac11e56821f3a
Nathen-Smith <nathencsmith@gmail.com>
2022-01-03 23:41:23 -0800

    a commit a day keeps the unemployment away

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon, UserIcon } from "@heroicons/react/outline";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Content from "./Content";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 const App = () => {
   const navigation = [
-    { name: "Home", to: "/" },
-    { name: "Search", to: "/search" },
-    { name: "Gallery", to: "/gallery" },
-    { name: "Description", to: "/description" },
+    { name: "Home", to: "#" },
+    { name: "Search", to: "#search" },
+    { name: "Gallery", to: "#gallery" },
+    { name: "Description", to: "#description" },
   ];
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setScrollTop(e.target.documentElement.scrollTop);
+      setScrolling(e.target.documentElement.scrollTop > scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+    console.log("height: " + window.innerHeight);
+    console.log(
+      document.getElementById("description")?.getBoundingClientRect().top
+    );
+    // we have the distance from the top of the page
+    // we can loop over all elements then set a property to true. needs to make sure only one on at once
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
 
   return (
     <div>
-      <Disclosure as="nav" className="bg-gray-100 h-16">
+      <Disclosure as="nav" className="bg-gray-100 h-16 sticky top-0 z-50">
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-              <div className="relative flex items-center justify-between h-16">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="flex items-center justify-between h-16">
+                <div className="inset-y-0 left-0 flex items-center sm:hidden">
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
@@ -42,7 +62,9 @@ const App = () => {
                             key={item.name}
                             href={item.to}
                             className={classNames(
-                              window.location.pathname === item.to
+                              window.location.href.slice(
+                                window.location.href.indexOf("#")
+                              ) === item.to
                                 ? "text-black"
                                 : "text-gray-400 hover:text-black",
                               "h-16 text-sm font-medium text-center flex justify-center items-center"
@@ -61,7 +83,11 @@ const App = () => {
                               },
                               {
                                 opacity:
-                                  window.location.pathname === item.to ? 1 : 0,
+                                  window.location.href.slice(
+                                    window.location.href.indexOf("#")
+                                  ) === item.to
+                                    ? 1
+                                    : 0,
                               })
                             }
                           ></div>
@@ -71,10 +97,10 @@ const App = () => {
                   </div>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <div className="ml-3 relative text-sm font-small flex flex-row">
+                  <div className="ml-3 relative text-sm font-small flex flex-row justify-center">
                     <div>Nathen</div>
                     <UserIcon className="h-5 w-5 text-black" />
-                    <FontAwesomeIcon icon={faGithub} />
+                    <FontAwesomeIcon icon={faGithub} className="h-5 w-5" />
                     {/* <span>Nathen</span> */}
                   </div>
                 </div>
@@ -87,7 +113,9 @@ const App = () => {
                     key={item.name}
                     href={item.to}
                     className={classNames(
-                      window.location.pathname === item.to
+                      window.location.href.slice(
+                        window.location.href.indexOf("#")
+                      ) === item.to
                         ? "bg-gray-300 text-black"
                         : "text-gray-400 hover:bg-gray-300 hover:text-black",
                       "px-3 py-2 rounded-md text-sm font-medium mr-auto"
@@ -101,6 +129,7 @@ const App = () => {
           </>
         )}
       </Disclosure>
+      <Content />
     </div>
   );
 };
---
+++ web/src/Content.jsx
@@ -0,0 +1,42 @@
+const Content = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div id="">Top</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div style={{ height: "100vh" }}></div>
+
+      <div id="description">Hi</div>
+    </div>
+  );
+};
+
+export default Content;

a5ef31e9a988a6c9db4848b3a598f9ae332dd124
Nathen-Smith <nathencsmith@gmail.com>
2022-01-04 01:07:30 -0800

    testing cd

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     { name: "Search", to: "#search" },
     { name: "Gallery", to: "#gallery" },
     { name: "Description", to: "#description" },
+    { name: "Experience", to: "#experience" },
   ];
 
   const [scrollTop, setScrollTop] = useState(0);

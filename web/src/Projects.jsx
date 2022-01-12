7a2a60b5e175ba4f3622a1cddf4be88b08ff2eaa
Nathen-Smith <nathencsmith@gmail.com>
2022-01-12 14:28:02 -0800

    undo project animation

--- web/src/Projects.jsx
+++ web/src/Projects.jsx
@@ -1,36 +1,14 @@
-import { InView } from "react-intersection-observer";
-import { useMediaQuery } from "react-responsive";
-
 const Projects = () => {
-  const isNotMobile = useMediaQuery({ query: "(min-width: 640px)" });
-
   return (
     <div id="portfolio" className="scroll-mt-16">
       <div className="text-center text-4xl">work i'm proud of</div>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-2 mx-10 sm:mx-0">
-        <InView triggerOnce={true}>
-          {({ ref, inView }) => (
-            <img
-              ref={ref}
-              src="/assets/childdevtracker.jpeg"
-              className="shadow-md"
-              style={
-                !isNotMobile
-                  ? inView
-                    ? {
-                        marginLeft: "0px",
-                        transition: "margin-left 0.6s",
-                      }
-                    : {
-                        marginLeft: "400px",
-                      }
-                  : {}
-              }
-              alt="childdevtracker"
-            />
-          )}
-        </InView>
+        <img
+          src="/assets/childdevtracker.jpeg"
+          className="shadow-md"
+          alt="childdevtracker"
+        />
 
         <video autoPlay muted loop>
           <source src="/assets/fpga_mario_nes.mov" type="video/mp4" />
